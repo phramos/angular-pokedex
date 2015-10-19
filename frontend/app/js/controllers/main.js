aae1c874f7bf0a926fd2271093f79a2c4ab99d4b
Pedro Souza <phramos@computacao.ufla.br>
2015-10-19 05:35:52 -0200

    Adding number to pokemon

--- frontend/app/js/controllers/main.js
+++ frontend/app/js/controllers/main.js
@@ -1,10 +1,19 @@
 pokedexApp.controller('MainCtrl', function ($scope) {
     $scope.pokemons = [
-        {'nome': 'Bulbasaur',
-            'descricao': 'The seed on its back is filled with nutrients. The seed grows steadily larger as its body grows.'},
-        {'nome': 'Ivysaur',
-            'descricao': 'The bulb on its back grows as it absorbs nutrients. The bulb gives off a pleasant aroma when it blooms.'},
-        {'nome': 'Venusaur',
-            'descricao': 'It is able to con vert sunlight into energy. As a result, it is more powerful in the summertime.'}
+        {
+            'numero': 1,
+            'nome': 'Bulbasaur',
+            'descricao': 'The seed on its back is filled with nutrients. The seed grows steadily larger as its body grows.'
+        },
+        {
+            'numero': 2,
+            'nome': 'Ivysaur',
+            'descricao': 'The bulb on its back grows as it absorbs nutrients. The bulb gives off a pleasant aroma when it blooms.'
+        },
+        {
+            'numero': 3,
+            'nome': 'Venusaur',
+            'descricao': 'It is able to con vert sunlight into energy. As a result, it is more powerful in the summertime.'
+        }
     ];
 });
