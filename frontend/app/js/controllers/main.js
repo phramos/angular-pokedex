0f00f6bcb985d3dddbfef496c19c07a28f05137b
Pedro Souza <phramos@computacao.ufla.br>
2015-10-21 04:38:54 -0200

    Using angular  built in funcionality

--- frontend/app/js/controllers/main.js
+++ frontend/app/js/controllers/main.js
@@ -1,21 +1,8 @@
-pokedexApp.controller('MainCtrl', function ($scope) {
-    $scope.pokemons = [
-        {
-            'numero': 1,
-            'nome': 'Bulbasaur',
-            'descricao': 'The seed on its back is filled with nutrients. The seed grows steadily larger as its body grows.'
-        },
-        {
-            'numero': 2,
-            'nome': 'Ivysaur',
-            'descricao': 'The bulb on its back grows as it absorbs nutrients. The bulb gives off a pleasant aroma when it blooms.'
-        },
-        {
-            'numero': 3,
-            'nome': 'Venusaur',
-            'descricao': 'It is able to con vert sunlight into energy. As a result, it is more powerful in the summertime.'
-        }
-    ];
+pokedexApp.controller('MainCtrl', function ($scope, $http) {
+
+    $http.get('../pokemons/pokemons.json').success(function (data) {
+        $scope.pokemons = data;
+    });
 
     $scope.ordem = 'numero';
 });
